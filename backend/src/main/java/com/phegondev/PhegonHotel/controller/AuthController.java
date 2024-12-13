58cc6ec5782dde5a5932d50720f6391dbbb33052
manoj-bhandare <manojbhandare2001@gmail.com>
2024-12-13 16:42:16 +0530

    final

---
+++ backend/src/main/java/com/phegondev/PhegonHotel/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.phegondev.PhegonHotel.controller;
+
+
+import com.phegondev.PhegonHotel.dto.LoginRequest;
+import com.phegondev.PhegonHotel.dto.Response;
+import com.phegondev.PhegonHotel.entity.User;
+import com.phegondev.PhegonHotel.service.interfac.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user) {
+
+        System.out.println("Debug...."+ user);
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest) {
+        Response response = userService.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
